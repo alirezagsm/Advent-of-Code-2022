d8faab33478d9090c829f764555064a09f7f13f6
alirezagsm <alirezaa.g@gmail.com>
2023-09-02 18:46:20 +0200

    day 6 completed

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -4,9 +4,13 @@ fn main() {
     let input = include_str!("input2.txt");
     let marker = get_marker(input, 4);
     println!("Part 1: {marker}");
+    let marker = get_marker_with_iterators(input, 4);
+    println!("Part 1: {marker}");
 
     let marker = get_marker(input, 14);
     println!("Part 2: {marker}");
+    let marker = get_marker_with_iterators(input, 14);
+    println!("Part 1: {marker}");
 }
 
 fn get_marker(input: &str, length: usize) -> usize {
@@ -21,6 +25,14 @@ fn get_marker(input: &str, length: usize) -> usize {
         buffer.push(c);
         buffer.remove(0);
     }
-    1
+    0
 }
 
+fn get_marker_with_iterators(input: &str, length: usize) -> usize {
+    let result = input.chars().collect::<Vec<_>>();
+    let result = result
+        .windows(length)
+        .enumerate()
+        .find(|(_i, buffer)| length == buffer.iter().collect::<HashSet<_>>().len());
+    result.unwrap().0 + length
+}
