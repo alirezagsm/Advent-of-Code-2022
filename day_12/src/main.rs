f66a8647889db799034cca2dbb31d79ebfd59b3f
alirezagsm <alirezaa.g@gmail.com>
2023-09-12 01:04:49 +0200

    update

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -22,54 +22,72 @@ fn main() {
     let mut current = start;
     let mut visited = vec![current];
 
-    let mut candidates = get_candidates(current, &map, &visited);
-    let mut nodes: Vec<(usize, usize)>;
-    let mut nodes_explored: Vec<Vec<(usize, usize)>>;
+    let mut nodes: Vec<(usize, usize)> = vec![];
+    let mut nodes_candidates: Vec<Vec<(usize, usize)>> = vec![];
     let mut min_path = 999;
+    let mut nclen = 0;
+    let mut candidates;
+    let mut back = false;
 
     loop {
         candidates = get_candidates(current, &map, &visited);
         if candidates.is_empty() {
-            match nodes.pop() {
-                Some(node) => {
-                    current = node;
-                    let mut history = current;
-                    while visited.last().unwrap() != &current {
-                        history = visited.pop().unwrap();
-                    }
-                    visited.pop();
-                    visited.push(history);
-                }
-                None => break,
+            if nodes.is_empty() {
+                break;
+            }
+            while visited.last().unwrap() != &nodes[nodes.len() - 1] {
+                visited.pop();
+            }
+            nclen = nodes_candidates.len();
+            if nodes_candidates[nclen - 1].len() == 0 {
+                nodes.pop();
+                nodes_candidates.pop();
+            } else if nodes_candidates[nclen - 1].len() > 0 {
+                current = nodes_candidates[nclen - 1].pop().unwrap();
             }
         } else {
-            // go back
-            if candidates.len() > 1 {
-                nodes.push(current);
-            }
             current = candidates.pop().unwrap();
-        };
+        }
 
+        if candidates.len() > 0 {
+            nodes.push(current);
+            nodes_candidates.push(candidates);
+        }
         visited.push(current);
-        show_map(&map, &visited);
-        if current == end && visited.len() < min_path {
-            min_path = visited.len();
+        if current == end {
             dbg!(&visited.len());
+            if visited.len() < min_path {
+                min_path = visited.len();
+                show_map(&map, &visited);
+                dbg!(&min_path);
+            }
         }
     }
 
-    println!("Part 1: {}", min_path - extras)
+    println!("Part 1: {}", min_path)
 }
 
 fn show_map(map: &Vec<Vec<u32>>, visited: &Vec<(usize, usize)>) {
-    // replace map element with 0 for visited
-    let mut map = map.clone();
-    for (i, j) in visited {
-        map[*i][*j] = 0;
+    // parse map into string
+    let mut map: Vec<Vec<String>> = map
+        .iter()
+        .map(|row| row.iter().map(|&c| c.to_string()).collect())
+        .collect();
+    let visited_len = visited.len();
+    for n in 0..visited_len - 2 {
+        if visited[n].0 as i32 == visited[n + 1].0 as i32 - 1 {
+            map[visited[n].0][visited[n].1] = "v".to_string();
+        } else if visited[n].0 as i32 == visited[n + 1].0 as i32 + 1 {
+            map[visited[n].0][visited[n].1] = "^".to_string();
+        } else if visited[n].1 as i32 == visited[n + 1].1 as i32 - 1 {
+            map[visited[n].0][visited[n].1] = ">".to_string();
+        } else if visited[n].1 as i32 == visited[n + 1].1 as i32 + 1 {
+            map[visited[n].0][visited[n].1] = "<".to_string();
+        }
     }
     for row in map {
         for col in row {
-            print!("{:5}", col);
+            print!("{:4}", col);
         }
         println!("");
     }
